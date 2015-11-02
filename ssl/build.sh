9072ac245d5199ed195a2729fee20779af390862
flames85 <flames85@163.com>
2015-11-02 16:24:44 +0800

    ssl和boost使用静态方式link

--- ssl/build.sh
+++ ssl/build.sh
@@ -1,4 +1,6 @@
-g++ -o server server.cpp -lboost_system -lboost_filesystem -lpthread -lssl -L${BOOSTDIR}/lib
+STATIC_LIBS="-lboost_system -lboost_filesystem -lssl -lcrypto -L./lib -lpthread -ldl -lrt"
 
-g++ -o client client.cpp -lboost_system -lboost_filesystem -lpthread -lssl -L${BOOSTDIR}/lib
+g++ -o server server.cpp $STATIC_LIBS
+
+g++ -o client client.cpp $STATIC_LIBS
 
